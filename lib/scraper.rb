d082356e73a4d2b1a99110d3d322cc53e8c5b453
Laila Campbell <lailacampbell96@gmail.com>
2020-03-28 23:10:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,7 +14,7 @@ class Scraper
   #uses a CSS selector to return an array of Nokogiri XML elements representing the courses described on the web page we are scraping" 
   
   def get_courses
-    doc.css = ".post"  
+    self.get_page.css(".post")
   end 
   
   
